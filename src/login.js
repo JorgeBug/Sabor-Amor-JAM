b44f21040f5647990b4e0b79692dd6dc2ed336f1
jorge <jorgeo.1225@gmail.com>
2023-02-13 10:53:29 -0600

    feat:CorrecionesLogin

--- src/login.js
+++ src/login.js
@@ -1,17 +1,71 @@
-const form = document.getElementById("form-login");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const saveInfoBtn = document.getElementById("saveBtn").addEventListener("click", saveData);
+const storedEmail = "jorgeo.1225@gmail.com";
+const storedPassword = "123";
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+localStorage.setItem(storedEmail, storedPassword);
 
-  checkInputs();
-});
+function enviar() {
+  const email = document.getElementById("emailid");
+  const password = document.getElementById("passwordid");
 
-function checkInputs() {
   const emailValue = email.value.trim();
   const passwordValue = password.value.trim();
+
+  if (emailValue === "") {
+    setErrorFor(email, "El email no puede estar en blanco");
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email, "El email no es válido");
+  } else if (emailValue !== storedEmail) {
+    setErrorFor(email, "El email ingresado no coincide con el registrado");
+  } else {
+    setSuccessFor(email);
+  }
+
+  if (passwordValue === "") {
+    setErrorFor(password, "La contraseña no puede estar en blanco");
+  } else if (!passwordCheck(passwordValue)) {
+    setErrorFor(
+      password,
+      "La contraseña debe tener mínimo 8 caracteres, 1 mayúscula, 1 minúscula y 1 número"
+    );
+  } else if (passwordValue !== storedPassword) {
+    setErrorFor(password, "La contraseña ingresada no coincide con la registrada");
+  } else {
+    setSuccessFor(password);
+  }
+
+  if (emailValue === storedEmail && passwordValue === storedPassword) {
+    location.href = "inicio.html";
+  }
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+
+  small.innerText = message;
+
+  formControl.className = "form-container error";
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-container success";
+}
+
+function isEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+}
+
+function passwordCheck(password) {
+  return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(password);
+}
+
+/*
+function checkInputs() {
+  const emailValue = email.value;
+  const passwordValue = password.value;
 
   if (emailValue === "") {
     setErrorFor(email, "El email no puede estar en blanco");
@@ -33,6 +87,14 @@ function checkInputs() {
   }
 
 }
+
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  checkInputs();
+});
+
 
 function setErrorFor(input, message) {
   const formControl = input.parentElement; //form-container
@@ -61,6 +123,8 @@ function passwordCheck(password) {
 function saveData() {
   const emailValue = email.value.trim();
   const passwordValue = password.value.trim();
+  saveBtn();
 }
 
 saveData();
+*/
