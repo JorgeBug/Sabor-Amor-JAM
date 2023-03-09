8094e978da22fff1da58b764653300f168a7dc37
Francisco BG <frank.betgar@gmail.com>
2023-03-09 13:33:20 -0600

    carrito en pag de producto

--- src/descripción.js
+++ src/descripción.js
@@ -17,9 +17,13 @@ fetch("https://sabor-amor.up.railway.app/api/catalogo")
         document.querySelector(".container-volume").textContent = `Contenido: ${filteredData.contenido} ml`;
         document.querySelector(".ingredients").textContent = `Descripción: ${filteredData.descripcion}`;
         document.querySelector(".price").textContent = `$${filteredData.precio}.00 MXN`;
-        document.querySelector("#add-btn").addEventListener("click", () => {
+        document.querySelector("#add-btn").setAttribute("data-name", filteredData.nombre);
+        document.querySelector("#add-btn").setAttribute("data-price", filteredData.precio);
+        document.querySelector("#add-btn").setAttribute("data-img", filteredData.imgLink);
+        document.querySelector("#add-btn").setAttribute("onclick","addToCart(event)");
+        /*document.querySelector("#add-btn").addEventListener("click",addToCart())*/ /*() => {
             // acción al hacer clic en el botón "AÑADIR"
-        });
+        });*/
 
     })
     .catch(error => console.error(error));
